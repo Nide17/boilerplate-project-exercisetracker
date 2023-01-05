405560fa332825ab7e004339c02e58dd29d81d52
Nide17 <nidedrogba@gmail.com>
2023-01-05 04:56:08 +0200

    refac

--- index.js
+++ index.js
@@ -12,14 +12,20 @@ app.use(cors())
 app.use(express.static('public'))
 
 // connect to the database
-mongoose.connect('mongodb+srv://parmenide:jesus123@fccmongoose.srblnut.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-
+mongoose.connect('mongodb+srv://parmenide:jesus123@fccmongoose.srblnut.mongodb.net/?retryWrites=true&w=majority',
+  err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB')
+  });
+  
+// Create default date
 const defaultDate = () => new Date().toISOString().slice(0, 10)
 
+// Create schema
 const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true, unique: false },
-    exercices: [
+    exercises: [
       {
         description: { type: String },
         duration: { type: Number },
@@ -28,120 +34,129 @@ const userSchema = mongoose.Schema(
     ]
   }
 )
+
+// Create model
 const User = mongoose.model('Users', userSchema)
 
-function addUser(req, res) {
+// Create default route
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+
+// Add user
+app.post("/api/users", (req, res) => {
+
   let username = req.body.username
+
   if (!username || username.length === 0) {
+
     res.json({ error: "Invalid username" })
   }
-  const user = new User({ username: username })
+  const user = new User({ username })
+
   user.save(function (err, newUser) {
+
     if (err) {
-      return console.log('addUser() error saving new user:', err)
+      return console.log(err)
     }
     res.json({ username: newUser.username, _id: newUser._id })
   })
-}
+})
 
-function getAllUsers(req, res) {
+// Get all users
+app.get("/api/users", (req, res) => {
+
   User.find()
+
     .select('username _id')
+
     .exec(function (err, userList) {
+
       if (err) {
-        return console.log('getAllUsers() error:', err)
+        return console.log(err)
       }
+
       res.json(userList)
     })
-}
+})
 
 
-function addExercise(req, res) {
-  const userId = req.params.userId || req.body.userId 
-  const exObj = {
-    description: req.body.description,
-    duration: +req.body.duration,
-    date: req.body.date || defaultDate()
-  } 
-  
-  
+// Add exercise
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const userId = req.params._id || req.body.userId
+
+
   User.findByIdAndUpdate(
     userId,
-    { $push: { exercices: exObj } },
+
+    {
+      $push: {
+
+        exercises: {
+          description: req.body.description,
+          duration: +req.body.duration,
+          date: req.body.date || defaultDate()
+        }
+      }
+    },
+
     { new: true },
+
     function (err, updatedUser) {
+
       if (err) {
         return console.log('error:', err)
       }
-      let returnObj = {
+
+      let userExer = {
         username: updatedUser.username,
-        description: exObj.description,
-        duration: exObj.duration,
+        description: req.body.description,
+        duration: +req.body.duration,
         _id: userId,
-        date: new Date(exObj.date).toDateString()
+        date: new Date(req.body.date).toDateString()
       }
-      res.json(returnObj)
+      res.json(userExer)
     }
   )
-}
+})
 
+// Get user logs
+app.get("/api/users/:_id/logs", (req, res) => {
 
-function getLog(req, res) {
-  let userId = req.params["_id"]
-  let dFrom = req.query.from || '0000-00-00'
-  let dTo = req.query.to || '9999-99-99'
+  let userId = req.params._id
+  let dateFrom = req.query.from || '0000-00-00'
+  let dateTo = req.query.to || '9999-99-99'
   let limit = +req.query.limit || 10000
 
   User.findOne({ _id: userId }, function (err, user) {
+
     if (err) {
-      return console.log('getLog() error:', err)
+      return console.log(err)
     }
+
     try {
-      let e1 = user.exercices.filter(e => e.date >= dFrom && e.date <= dTo)
-      let e2 = e1.map(e => (
-        {
-          description: e.description,
-          duration: e.duration,
-          date: e.date
-        }
-      ))
-      let ex = user.exercices.filter(e => e.date >= dFrom && e.date <= dTo)
-        .map(e => (
-          {
-            description: e.description,
-            duration: e.duration,
-            date: e.date
-          }
-        ))
+      let exer = user.exercises.filter(exc => exc.date >= dateFrom && exc.date <= dateTo)
+        .map(exc =>
+        ({
+          description: exc.description,
+          duration: exc.duration,
+          date: exc.date
+        }))
         .slice(0, limit)
-      let logObj = {}
-      logObj.count = ex.length
-      logObj._id = user._id
-      logObj.username = user.username
-      logObj.log = ex
-      res.json(logObj)
+
+      res.json({
+        count: exer.length,
+        _id: user._id,
+        username: user.username,
+        log: exer
+      })
 
     } catch (err) {
       console.log(err)
-      res.json(ERR_USER_NOTFUND)
+
+      res.json("User not found")
     }
+
   })
-}
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
-
-app.post("/api/users", addUser)
-app.post("/api/exercise/new-user", addUser)
-
-app.get("/api/users", getAllUsers)
-app.get("/api/exercise/users", getAllUsers)
-
-app.post("/api/exercise/add", addExercise) 
-
-app.all("/api/users/:userId/exercises", addExercise) 
-
-app.get("/api/exercises/:userId/log", getLog)
-app.get("/api/users/:_id/logs", getLog)
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
